ed6cf300f01d2d451c8ee69285bc32a580aeb71e
DC11 <dmytrochobit11@gmail.com>
2021-03-03 23:17:02 +0200

    frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeamListComponent } from './components/team-list/team-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import { TeamDetailsComponent } from './components/team-details/team-details.component';
+import { UpdateTeamComponent } from './components/update-team/update-team.component';
+import { PlayerListComponent } from './components/player-list/player-list.component';
+import { PlayerDetailsComponent } from './components/player-details/player-details.component';
+import { UpdatePlayerComponent } from './components/update-player/update-player.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamListComponent,
+    TeamDetailsComponent,
+    UpdateTeamComponent,
+    PlayerListComponent,
+    PlayerDetailsComponent,
+    UpdatePlayerComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
